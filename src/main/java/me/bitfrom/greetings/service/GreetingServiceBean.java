3f4c26baa3f49d9282c9e1c224e9c62a008168ee
Constantine Chernenko <constantine.chernenko@gmail.com>
2016-07-26 16:33:01 +0300

    In-memory Guava cache implemented

--- src/main/java/me/bitfrom/greetings/service/GreetingServiceBean.java
+++ src/main/java/me/bitfrom/greetings/service/GreetingServiceBean.java
@@ -3,11 +3,17 @@ package me.bitfrom.greetings.service;
 import me.bitfrom.greetings.model.Greeting;
 import me.bitfrom.greetings.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
     private final GreetingRepository greetingRepository;
@@ -23,11 +29,14 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Cacheable(value = "greetings", key = "#id")
     public Greeting findOne(Long id) {
         return greetingRepository.findOne(id);
     }
 
     @Override
+    @CachePut(value = "greetings", key = "#result.id")
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Greeting create(Greeting greeting) {
         if (greeting.getId() != null) {
             //Can't create Greeting with specified id value
@@ -37,6 +46,8 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @CachePut(value = "greetings", key = "#greeting.id")
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public Greeting update(Greeting greeting) {
         Greeting persistedGreeting = findOne(greeting.getId());
         if (persistedGreeting == null) {
@@ -47,7 +58,15 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @CacheEvict(value = "greetings", key = "#id")
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public void delete(Long id) {
         greetingRepository.delete(id);
+    }
+
+    @Override
+    @CacheEvict(value = "greetings", allEntries = true)
+    public void evictCache() {
+
     }
 }
